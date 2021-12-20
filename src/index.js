968b58a1fa78c998a8f3134fe2715fc497d2ca5f
ReyMon <ramon@reymon.mx>
2021-12-20 12:29:52 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ var UserList = {
       ),
       m('tbody', 
             m('tr', m('td', 'm-view'), m('td', {title='Sorpresa'}, 'CURP_ALUMNO'), m('td', {title='CHAR(25)'}, 'NOMBRES'))
-       )
+       ))
     }
 }
 
