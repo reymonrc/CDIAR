235eca8226ae772a685bd8001be70a2f45c0e78c
ReyMon <ramon@reymon.mx>
2021-12-20 12:57:50 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ var List = {
         )
       ),
       m('tbody', 
-            m('tr', m('td', 'PREINSCRIPCIONES_DATOS'), m('td', {title='VARCHAR2(18 BYTE)'}, 'CURP'), m('td', {title='VARCHAR2(64 BYTE)'}, 'NOMBRES'))
+            m('tr', m('td', 'PREINSCRIPCIONES_DATOS'), m('td', {title:'VARCHAR2(18 BYTE)'}, 'CURP'), m('td', {title:'VARCHAR2(64 BYTE)'}, 'NOMBRES'))
        ))
     }
 }
